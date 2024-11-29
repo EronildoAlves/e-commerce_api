4314a660b053227e41630596fd9a2d8c54c5f3db
Eronildo Nascimento <eronildof36@gmail.com>
2024-11-29 17:15:19 -0300

    project created

---
+++ ecommerce/src/main/java/com/nascimento/ecommerce/model/Product.java
@@ -0,0 +1,28 @@
+package com.nascimento.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Product")
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+}
+
+
